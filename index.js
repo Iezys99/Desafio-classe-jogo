4bc46a53a7ba1c1195abfadeca4d0d1f4cd5f5c2
Iezys99 <igo.iezys@hotmail.com>
2025-03-19 23:42:22 -0300

    second commit

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
-class hero{
-    constructor (nome, idade, tipo){
+class Hero {
+    constructor(nome, idade, tipo) {
         this.nome = nome
         this.idade = idade
         this.tipo = tipo
     }
-​
-    atacar (ataque){
-        switch (this.tipo){
+
+    atacar() {
+        switch (this.tipo) {
             case "mago":
-            this.ataque = "magia"
-            break
-            
+                this.ataque = "magia"
+                break
             case "guerreiro":
-            this.ataque = "espada"
-            break
-​
+                this.ataque = "espada"
+                break
             case "monge":
-            this.ataque = "artes marciais"
-            break
-​
+                this.ataque = "artes marciais"
+                break
             case "ninja":
-            this.ataque = "shuriken"
-            break
+                this.ataque = "shuriken"
+                break
         }
     }
 }
-​
-let heroAlfa = new hero ("Igor", 28, "mago")
-let heroBeta = new hero ("Juh", 22, "ninja")
-let heroGama = new hero ("Chaw", 31, "guerreiro")
-​
+
+
+let heroAlfa = new Hero("Igor", 28, "mago")
+let heroBeta = new Hero("Juh", 22, "ninja")
+let heroGama = new Hero("Chaw", 31, "guerreiro")
+
+
 heroAlfa.atacar()
 heroBeta.atacar()
 heroGama.atacar()
-​
-console.log (`O ${heroAlfa.tipo} atacou usando ${heroAlfa.ataque}`)
-console.log (`O ${heroBeta.tipo} atacou usando ${heroBeta.ataque}`)
-console.log (`O ${heroGama.tipo} atacou usando ${heroGama.ataque}`)
+
+console.log(`O ${heroAlfa.tipo} atacou usando ${heroAlfa.ataque}`)
+console.log(`O ${heroBeta.tipo} atacou usando ${heroBeta.ataque}`)
+console.log(`O ${heroGama.tipo} atacou usando ${heroGama.ataque}`)
